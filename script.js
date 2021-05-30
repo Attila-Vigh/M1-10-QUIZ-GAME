ea8f9fd46bdefac97b4de74204416234d37bce82
attila <attilavigh.contact@gmail.com>
2021-05-30 11:31:35 +0100

    Refactory: Randomised answers moved separate function getRandomAnwers()

--- script.js
+++ script.js
@@ -10,43 +10,42 @@ const getQuziData = async function() {
     .then( result => result.json())
     .then( result => result.results)
     // .then( result => console.log(result))
-    .catch( err => console.log(err))
-                                                        console.log("quiz ", quiz) // TODO: delete
+    .catch( err => console.log(err));                   console.log("getQuziData: quiz ", quiz) // TODO: delete
+
     return quiz
 }
-                                                    
-const displayQuiz = async function(){
-        
-    const quiz = await getQuziData()
-                                                        console.log("quiz ", quiz) // TODO: delete
-    let ol = document.querySelector("ol")
-    for( const q of quiz ){
-        
-        const {
-            category,
-            correct_answer,
-            difficulty,
-            incorrect_answers,
-            question,
-            type
-        } = q
 
-        const answers = incorrect_answers.concat(correct_answer)
+const getRandomAnwers = ( incorrect_answers, correct_answer) => {
+    
+    const answers = incorrect_answers.concat(correct_answer)
+    
+    return answers.sort(() => Math.random() - 0.5)
+}
+
+const displayQuiz = async function(){
+    
+    const quizData = await getQuziData();                   console.log("displayQuiz: quizData ", quizData) // TODO: delete
+    const ol   = document.querySelector("ol")
+    
+    for( const q of quizData ){
         
-        const randomAnwers = answers.sort(() => Math.random() - 0.5)
+        const { category, correct_answer, difficulty, incorrect_answers, question, type } = q
+
+        const randomAnwers = getRandomAnwers( incorrect_answers, correct_answer)
+
         let radioInput = []        
-        for (const answer of randomAnwers) {
+        for (const randomAnwer of randomAnwers) {
             radioInput 
             += `
                 <input  
                     type="radio" 
-                    id=${answer}
+                    id=${randomAnwer}
                     name=${correct_answer}
                 >
-                <label for=${answer}>${answer}</label>
+                <label for=${randomAnwer}>${randomAnwer}</label>
                 `
-                                    console.log( answer);
-                                    console.log(radioInput); // TODO: check
+                                    console.log( randomAnwer);// TODO:
+                                    console.log(radioInput); // TODO:
         }
         
         ol.innerHTML
