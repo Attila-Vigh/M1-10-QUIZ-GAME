9321790941685b9e8884698406a8fa8881ebb9cd
attila <attilavigh.contact@gmail.com>
2021-05-30 12:07:58 +0100

    Refactory: created displayRadioInput()
    Input element/data moved out of  displayQuiz() and moved into new created function displayRadioInput ()

--- script.js
+++ script.js
@@ -21,33 +21,38 @@ const getRandomAnwers = ( incorrect_answers, correct_answer) => {
     
     return answers.sort(() => Math.random() - 0.5)
 }
+const displayRadioInput = (randomAnwers, inputName) => {
+
+    let radioInput = []        
+    for (const randomAnwer of randomAnwers) {
+        radioInput 
+        += `
+            <input  
+                type="radio" 
+                id=${randomAnwer}
+                name=${inputName}
+            >
+            <label for=${randomAnwer}>${randomAnwer}</label>
+            `
+                                console.log( randomAnwer);// TODO:
+                                console.log(radioInput); // TODO:
+    }
+    return radioInput
+}
 
 const displayQuiz = async function(){
     
-    const quizData = await getQuziData();                   console.log("displayQuiz: quizData ", quizData) // TODO: delete
-    const ol   = document.querySelector("ol")
-    
+        const quizData = await getQuziData();                   console.log("displayQuiz: quizData ", quizData) // TODO: delete
+        
+        const ol   = document.querySelector("ol")
     for( const q of quizData ){
         
         const { category, correct_answer, difficulty, incorrect_answers, question, type } = q
 
         const randomAnwers = getRandomAnwers( incorrect_answers, correct_answer)
 
-        let radioInput = []        
-        for (const randomAnwer of randomAnwers) {
-            radioInput 
-            += `
-                <input  
-                    type="radio" 
-                    id=${randomAnwer}
-                    name=${correct_answer}
-                >
-                <label for=${randomAnwer}>${randomAnwer}</label>
-                `
-                                    console.log( randomAnwer);// TODO:
-                                    console.log(radioInput); // TODO:
-        }
-        
+        const radioInput = displayRadioInput(randomAnwers, correct_answer)
+
         ol.innerHTML
         +=  `
                 <li>
